bc92794feff010d584416c25750ff8e17e7d3b70
jinwoo <jinwoo8405@naver.com>
2023-04-24 11:11:57 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
